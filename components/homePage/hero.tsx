d2fd0d947a4994c11a05f262965976e9b53761aa
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2022-01-07 23:20:49 +0100

    Add responsive to hero

--- components/homePage/hero.tsx
+++ components/homePage/hero.tsx
@@ -5,9 +5,9 @@ export const Hero = () => {
     <section className={classes.container}>
       <div className={classes.wave}></div>
 
-      <div className="p-8 text-white">
+      <div className="p-12 text-white">
         <h1 className="text-5xl">Hi! I`m Felipe Ruiz</h1>
-        <p className="my-8 w-3/5">
+        <p className="my-12 md:w-2/5">
           Full Stack web developer with more than 6 years of experience working
           with technologies such as React, NextJS, Typescript and much more
         </p>
