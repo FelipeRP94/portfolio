77d40c6d25a5e1ae51048aaa4509af7bf91b8752
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2022-01-08 19:41:36 +0100

    Update hero style

--- components/homePage/hero/hero.tsx
+++ components/homePage/hero/hero.tsx
@@ -1,17 +1,11 @@
-import classes from "./hero.module.css";
-
-export const Hero = () => {
-  return (
-    <section className={classes.container}>
-      <div className={classes.wave}></div>
-
-      <div className="p-12 text-white">
-        <h1 className="text-5xl">Hi! I`m Felipe Ruiz</h1>
-        <p className="my-12 md:w-2/5">
-          Full Stack web developer with more than 6 years of experience working
-          with technologies such as React, NextJS, Typescript and much more
-        </p>
-      </div>
-    </section>
-  );
-};
+export const Hero = () => (
+  <section className="bg-sky-900">
+    <div className="p-12 text-white">
+      <h1 className="text-5xl">Hi! I`m Felipe Ruiz</h1>
+      <p className="my-12 md:w-2/5">
+        Full Stack web developer with more than 6 years of experience working
+        with technologies such as React, NextJS, Typescript and much more
+      </p>
+    </div>
+  </section>
+);
