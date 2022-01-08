b4bdbac6c394ae09b1da6e9eaf2f5de225724299
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2022-01-08 20:03:38 +0100

    Add meta tags and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Fragment } from "react";
 import { Header } from "../components/layout/header";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Fragment>
       <Header />
+      <Head>
+        <title>Felipe Ruiz Pinto</title>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Full Stack Web Developer" />
+      </Head>
       <Component {...pageProps} />
     </Fragment>
   );
