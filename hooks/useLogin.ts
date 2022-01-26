0f40c728def4cf448f19eefb58dbca9e123d4b10
Felipe Ruiz Pinto <felipe.r.p.1994@gmail.com>
2022-01-26 23:04:28 +0100

    Add client side validation in login form

--- hooks/useLogin.ts
+++ hooks/useLogin.ts
@@ -11,6 +11,16 @@ export const useLogin = () => {
   const submitLogin = async (event: any) => {
     event.preventDefault();
 
+    if (username === "") {
+      setError("Username is required");
+      return;
+    }
+
+    if (password === "") {
+      setError("Password is required");
+      return;
+    }
+
     const { error }: any = await signIn("credentials", {
       username,
       password,
